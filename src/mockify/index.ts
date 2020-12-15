89c114df074ac7c405214e37fea85aa2e167c3c2
Nate Kumar <nk@neoftl.com>
2020-12-15 07:52:00 +0000

    Add npm build:watch script + Schema type

--- src/mockify/index.ts
+++ src/mockify/index.ts
@@ -1,10 +1,14 @@
+import { Schema } from './schema.d';
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function mockify(_options: any): Rule {
+export function mockify(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+
+    console.log( {tree} )
+
     return tree;
   };
 }
