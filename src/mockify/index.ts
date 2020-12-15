61ac3ba6dda0b187395fbcbb1f23b5e9e285774b
Nate Kumar <nk@neoftl.com>
2020-12-15 07:58:55 +0000

    Formatting

--- src/mockify/index.ts
+++ src/mockify/index.ts
@@ -4,9 +4,11 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function mockify(_options: Schema): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-
+export function mockify( _options: Schema ): Rule {
+  return (
+    tree: Tree,
+    _context: SchematicContext
+  ) => {
     console.log( {tree} )
 
     return tree;
